12e3cd84bfa7c5a0aac26596b77ec398b095146d
Jimmy Feng <jfeng1996@gmail.com>
2021-04-20 12:31:36 -0400

    Added basemap function to GUI

--- hagerstrand/toolbar.py
+++ hagerstrand/toolbar.py
@@ -9,6 +9,52 @@ from ipyfilechooser import FileChooser
 from IPython.display import display
 
 
+def change_basemap(m):
+    """Widget for change basemaps. Source: Dr. Qiusheng Wu -- https://github.com/giswqs/geemap/blob/master/geemap/toolbar.py
+    Args:
+        m (object): hagerstrand.Map()
+    """
+    from .basemaps import _ee_basemaps
+
+    dropdown = widgets.Dropdown(
+        options=list(_ee_basemaps.keys()),
+        value="ROADMAP",
+        layout=widgets.Layout(width="200px"),
+        description="Basemaps",
+    )
+
+    close_btn = widgets.Button(
+        icon="times",
+        tooltip="Close the basemap widget",
+        button_style="primary",
+        layout=widgets.Layout(width="32px"),
+    )
+
+    basemap_widget = widgets.HBox([dropdown, close_btn])
+
+    def on_click(change):
+        basemap_name = change["new"]
+
+        if len(m.layers) == 1:
+            old_basemap = m.layers[0]
+        else:
+            old_basemap = m.layers[1]
+        m.substitute_layer(old_basemap, _ee_basemaps[basemap_name])
+
+    dropdown.observe(on_click, "value")
+
+    def close_click(change):
+        m.toolbar_reset()
+        if m.basemap_ctrl is not None and m.basemap_ctrl in m.controls:
+            m.remove_control(m.basemap_ctrl)
+        basemap_widget.close()
+
+    close_btn.on_click(close_click)
+
+    basemap_control = WidgetControl(widget=basemap_widget, position="topright")
+    m.add_control(basemap_control)
+    m.basemap_ctrl = basemap_control
+
 def main_toolbar(m):
 
     padding = "0px 0px 0px 5px"  # upper, right, bottom, left
@@ -42,7 +88,7 @@ def main_toolbar(m):
     cols = 2
     grid = widgets.GridspecLayout(rows, cols, grid_gap="0px", layout=widgets.Layout(width="62px"))
 
-    icons = ["folder-open", "map", "gears", "question"]
+    icons = ["folder-open", "map", "gears", "filter"]
 
     for i in range(rows):
         for j in range(cols):
@@ -76,9 +122,10 @@ def main_toolbar(m):
 
     data_dir = os.path.abspath('./data')
 
+    # File Chooser Widget
     fc = FileChooser(data_dir)
     fc.use_dir_icons = True
-    fc.filter_pattern = ['*.shp', '*.geojson']
+    fc.filter_pattern = ['*.shp', '*.geojson', '*.json']
 
     filechooser_widget = widgets.VBox([fc, buttons])
 
@@ -95,12 +142,61 @@ def main_toolbar(m):
             m.remove_control(output_ctrl)
     buttons.observe(button_click, "value")     
 
+    # Basemap Widget
+    from .basemaps import _ee_basemaps
+
+    dropdown_basemap = widgets.Dropdown(
+        options=list(_ee_basemaps.keys()),
+        value="ROADMAP",
+        layout=widgets.Layout(width="200px"),
+        description="Basemaps",
+    )
+
+    close_button_basemap = widgets.Button(
+        icon="times",
+        tooltip="Close the basemap widget",
+        button_style="primary",
+        layout=widgets.Layout(width="32px"),
+    )
+
+    basemap_widget = widgets.HBox([dropdown_basemap, close_button_basemap])
+
+    def on_click(change):
+        basemap_name = change["new"]
+
+        if len(m.layers) == 1:
+            old_basemap = m.layers[0]
+        else:
+            old_basemap = m.layers[1]
+        m.substitute_layer(old_basemap, _ee_basemaps[basemap_name])
+
+    dropdown_basemap.observe(on_click, "value")
+
+    def close_click(change):
+        m.toolbar_reset()
+        if m.basemap_ctrl is not None and m.basemap_ctrl in m.controls:
+            m.remove_control(m.basemap_ctrl)
+        basemap_widget.close()
+
+    close_button_basemap.on_click(close_click)
+
+    basemap_control = WidgetControl(widget=basemap_widget, position="topright")
+ #   m.add_control(basemap_control)
+    m.basemap_ctrl = basemap_control
+
     def tool_click(b):    
         with output:
             output.clear_output()
             if b.icon == "folder-open":
                 display(filechooser_widget)
                 m.add_control(output_ctrl)
+
+ #           elif b.icon == "filter":
+
+            elif b.icon == "map":
+                display(basemap_widget)
+                m.add_control(basemap_control)
+
             elif b.icon == "gears":
                 import whiteboxgui.whiteboxgui as wbt
 
@@ -119,6 +215,12 @@ def main_toolbar(m):
 
                 m.whitebox = wbt_control
                 m.add_control(wbt_control)
+
+#            elif b.icon == "question":
+
+            print(f"You clicked the {b.icon} button")
+
+
 
     for i in range(rows):
         for j in range(cols):
